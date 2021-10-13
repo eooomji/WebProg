5c956faa9de7935a2228084efe1b043c1a00c35e
eooomji <70639677+eooomji@users.noreply.github.com>
2021-10-13 12:16:04 +0900

    1번

---
+++ 7주차/01.js
@@ -0,0 +1,8 @@
+const isOktoDrink = (age) => {
+    if (age > 18) return true;
+    else return false;
+};
+
+console.log(isOktoDrink(24));
+console.log(isOktoDrink(19));
+console.log(isOktoDrink(18));
