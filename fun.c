887af741dd246baaf3ffe0547c7c9c6c137b24dd
엄지 <70639677+eooomji@users.noreply.github.com>
2021-11-29 18:42:52 +0900

    map(), filter() 실습 문제

---
+++ fun.c
@@ -0,0 +1,10 @@
+// map
+console.log("=========== map 함수 =========== ");
+const arr1 = [65, 44, 12, 4];
+const arr2 = arr1.map(arr => arr*10);
+console.log(arr2);
+
+// filter
+console.log("=========== filter 함수 =========== ");
+const arr3 = arr1.filter(arr => arr >= 30);
+console.log(arr3);
